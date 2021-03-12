8449f7e5ac093bb78dc9d4e42d17cac0b9e202c9
xurimx <xurimx@gmail.com>
2021-03-12 11:13:51 +0100

    removed while loop in worker

--- DemoReceiver.Worker/Worker.cs
+++ DemoReceiver.Worker/Worker.cs
@@ -19,11 +19,7 @@ namespace DemoReceiver.Worker
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
-            }
+            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
         }
     }
 }
